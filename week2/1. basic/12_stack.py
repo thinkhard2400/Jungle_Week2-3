e750c63e238b45a8a44ce5066a7124f86d8cac7f
Lacri <thinkhard2400@gmail.com>
2026-08-30 21:22:37 +0900

    [Solved]

--- week2/1. basic/12_stack.py
+++ week2/1. basic/12_stack.py
@@ -1,62 +1,17 @@
-"""
-[스택 - 괄호 짝 맞추기]
-
-문제 설명:
-- 스택(Stack)을 사용하여 괄호가 올바르게 짝지어져 있는지 확인합니다.
-- LIFO (Last In First Out) 구조를 활용합니다.
-
-입력:
-- s: 괄호 문자열 (예: "(())", "(()")
-
-출력:
-- True: 올바른 괄호
-- False: 잘못된 괄호
-
-예제:
-입력: "(())"
-출력: True
-
-입력: "(()"
-출력: False
-
-힌트:
-- 여는 괄호 '('는 스택에 push
-- 닫는 괄호 ')'를 만나면 스택에서 pop
-- 마지막에 스택이 비어있으면 True
-"""
-
 def is_valid_parentheses(s):
-    """
-    괄호 짝이 맞는지 확인
-    
-    Args:
-        s: 괄호 문자열
-    
-    Returns:
-        올바른 괄호면 True, 아니면 False
-    """
-    stack = []
-    
-    # TODO: 문자열의 각 문자를 순회
-    ## : 여는 괄호 '('면 스택에 추가
-    ## : 닫는 괄호 ')'면
-    ## 스택이 비어있으면 False 반환
-    ## 아니면 스택에서 pop
-    pass
-    
-    # TODO: 반복이 끝나면 스택이 비어있는지 확인
-    pass
 
-    for char in s:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
-            if not stack:
+    clean_list = []
+
+    for i in s:
+        if i == '(':
+            clean_list.append(i)
+        elif i == ')':
+            if not clean_list:
                 return False
             else:
-                stack.pop()
+                clean_list.pop()
 
-    if not stack:
+    if not clean_list:
         return True
     else:
         return False
