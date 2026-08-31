373d009594c1b43c52b01a511b7a38cca62d7da6
Lacri <thinkhard2400@gmail.com>
2026-08-31 11:52:45 +0900

    [Solved]

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -1,82 +1,21 @@
-"""
-[버블 정렬 구현]
-
-문제 설명:
-- 버블 정렬(Bubble Sort) 알고리즘을 구현합니다.
-- 인접한 두 원소를 비교하여 정렬하는 방식입니다.
-- 가장 큰 원소가 배열의 끝으로 "버블"처럼 이동합니다.
-
-입력:
-- arr: 정렬되지 않은 정수 배열
-
-출력:
-- 오름차순으로 정렬된 배열
-
-예제:
-입력: [64, 34, 25, 12, 22, 11, 90]
-출력: [11, 12, 22, 25, 34, 64, 90]
-
-힌트:
-- 외부 반복문: n-1번 실행
-- 내부 반복문: 인접한 원소 비교 및 교환
-- 최적화: 교환이 없으면 이미 정렬된 것이므로 조기 종료
-"""
-
 def bubble_sort(arr):
-    """
-    버블 정렬 구현
-    
-    Args:
-        arr: 정렬할 배열
-    
-    Returns:
-        정렬된 배열
-    """
-    # TODO: 외부 반복문 - n-1번 반복
-    # 각 패스마다 가장 큰 원소가 끝으로 이동
-    ## TODO: 내부 반복문 - 인접한 원소 비교
-    ## 0부터 n-i-1까지 반복 (이미 정렬된 뒷부분 제외)
-    ## TODO: 인접한 두 원소 비교 및 교환
-    ## arr[j] > arr[j+1]이면 교환
-    ## 외부 반복문: n-1번 실행
-    pass
     n = len(arr)
-
     for i in range(n-1):
         for j in range(n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
     return arr
 
 def bubble_sort_optimized(arr):
-    """
-    최적화된 버블 정렬 (조기 종료 포함)
-    
-    Args:
-        arr: 정렬할 배열
-    
-    Returns:
-        정렬된 배열
-    """
-    n = len(arr)
-    
+    n = len(arr)    
     for i in range(n):
-        swapped = False  # 교환 발생 여부
+        swapped = False
         for j in range(n-1-i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
         if not swapped:
-            break        
-        # TODO: 내부 반복문과 교환 로직 구현
-        # 교환이 발생하면 swapped = True 설정        
-        pass
-        
-
-        # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
-
+            break
     return arr
 
 # 테스트 케이스
